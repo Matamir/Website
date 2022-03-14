4b8d497b015a51b26f3919b87e3f1e147932d496
Matamir <mhirsch217@gmail.com>
2022-03-14 03:34:11 -0400

    set up majority of website, and added first few projects

--- public/Projects/Project.js
+++ public/Projects/Project.js
@@ -1,4 +1,43 @@
 export default [
-  {projectName:'First Project',projectLanguages:'Java, Javascript',projectDescription:'My first project',projectImage:''},
-  {projectName:'Second Project',projectLanguages:'HTML, CSS',projectDescription:'My second project',projectImage:''},
+  {projectName:`This Website`,
+    projectLanguages:`HTML, CSS, Javascript`,
+    projectDescription:`Using techniques learned in my web development course, I created this website to show off projects that I work on, 
+    and to create a hub from which I can link my most recent resume as well as my github and linkedIn accounts.`,
+    projectImage:"",
+    projectTimeline:`March 2022`},
+
+  {projectName:`Discord Bot`,
+    projectLanguages:`Python`,
+    projectDescription:`Using the discord.py API, I wrote a simple discord bot that could respond to messages.
+    The bot had different responses based on key words or the sender of a message, as well as fully random responses.
+    The bot was hosted using an always-on repl.it
+    This required reading though fairly extensive documentation, as well as testing in a discord server to find a good frequency for the bot to respond.`,
+    projectImage:``,
+    projectTimeline:`July-August 2021`},
+
+  {projectName:`Photo Editor`,
+    projectLanguages:`Java`,
+    projectDescription:`Worked with a partner to create a photo editor following the Model-View-Controller design pattern and Object-Oriented-Design principles.
+    Features included: import/export functionality from various file formats, image filtering (including blur, sharpen, grayscale, sepia, and mosaic), image layering, and the ability to execute scripts.
+    We also created our own image file format very similar to PPM which could be interpreted by our program and could be imported and exported.
+    Finally we created a very simple GUI from which these features could be used.
+    This project was done over the course of about a month.  Unfortunately we were asked not to make a public github repository, but a private repository is available upon request.`,
+    projectImage:"../public/Images/PhotoEditor.png",
+    projectTimeline:`June 2021`},
+
+  {projectName:`Flood-it`,
+    projectLanguages:`Java`,
+    projectDescription:`Created Flood-it game in Java with a selectable board size from 1x1 to 64x64, two player functionality, and record keeping for best time and fewest clicks.
+    The board randomly generates once the size is selected, coloring in each position with one of five colors (the game could take any non-zero number of colors, but the assignment called for five).
+    The program also generates a goal number of clicks for the player to fill the whole board, based on the board size they chose.  If that goal is met, the user score is set as the record to beat, and the new click goal.
+    This project was based on the example found <a href="https://unixpapa.com/floodit/" target="_blank">here</a>.
+    Unfortunately we were asked not to make a public github repository, but a private repository is available upon request.`,
+    projectImage:"../public/Images/floodIt.png",
+    projectTimeline:`March-April 2021`},
+
+  {projectName:``,
+    projectLanguages:``,
+    projectDescription:``,
+    projectImage:"",
+    projectTimeline:``},
 ];
--- public/Projects/ProjectsListItem.js
+++ public/Projects/ProjectsListItem.js
@@ -1,16 +1,31 @@
 const ProjectListItem = (project) => {
+
+  // This conditional make it so the cant load image icon doesn't come up if no image is provided.
+  var imageHTML = ""
+  if(project.projectImage !== ""){
+    imageHTML =
+          `<div class="column">
+            <img src=${project.projectImage} class="img-fluid" alt="Responsive Image" style="width:384px;>
+          </div>`
+  }
   return(`
-        <div class="container">
-          <div class="row">
-          
-            <img src=${project.projectImage}>
+      <div class="container my-5">
+        <div class="row">
+          ` + imageHTML +
+          `
+        </div>
+        <div class="row pt-1">
+          <div class="column"><p class="font-weight-bold pr-3">${project.projectName}</p> </div>
+          <div class="column"><p class="font-italic pr-5">${project.projectLanguages}</p></div>
+          <div class="column"><p class="font-weight-light pl-5">${project.projectTimeline}</p></div>
+
             
-            <p class="font-weight-bold">${project.projectName}</p>  
-            <p class="font-italic">${project.projectLanguages}</p>  
+            
            
             <div>${project.projectDescription}</div>
-          </div>  
+           <br>
         </div>
+      </div>
       `);
 }
 export default ProjectListItem;
