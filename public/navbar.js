4d2b7d49a57112e55325c6585219374c99aa14bc
Matamir <mhirsch217@gmail.com>
2024-02-02 12:40:27 -0500

    updating navbar to be a more usable size

--- public/navbar.js
+++ public/navbar.js
@@ -5,39 +5,42 @@ import ProjectList from "./Projects/index.js";
           <nav class="navbar fixed-top" style="background-color: #F5A9B8dd">
     <div class="container p-2">
 
-      <div class="col-5 text-white">
+      <div class="col-6 text-white" style="font-size:30px">
         Mathew Hirsch Portfolio Website
       </div>
       
-      
+<!--
       <div hidden class="col-1">
-        <a href="./index.html" class="text-white">
+        <a href="./index.html" class="text-white" style="font-size:30px">
           Home
         </a>
       </div>
-      
+-->
+
+
       <div class="col-1">
-        <a href="./about.html" class="text-white">
+        <a href="./about.html" class="text-white" style="font-size:30px">
           About
         </a>
       </div>
+
 <!-- 
       <div class="col-1">
-        <a href="./demos.html" class="text-white">
+        <a href="./demos.html" class="text-white" style="font-size:30px">
           Demos
         </a>
       </div>
 -->
 
       <div class="col-1">
-        <a href="./projectsPage.html" class="text-white">
+        <a href="./projectsPage.html" class="text-white" style="font-size:30px">
           Projects
         </a>
       </div>
 
 
       <div class="col-1">
-        <a href="./Resume.pdf" class="text-white" target="_blank">
+        <a href="./Resume.pdf" class="text-white" target="_blank" style="font-size:30px">
           Resume
         </a>
       </div>
@@ -45,13 +48,13 @@ import ProjectList from "./Projects/index.js";
 
       <div class="col-1">
         <a href="mailto:hirsch.m@northeastern.edu" class="text-white" target="_blank">
-          <i class="fas fa-envelope"></i>
+          <i class="fas fa-envelope">  email  </i>
         </a>
         <a href="https://www.linkedin.com/in/mathew-hirsch/" class="text-white" target="_blank">
-          <i class="fab fa-linkedin"></i>
+        <i class="fab"> linked </i> <i class="fab fa-linkedin"></i>
         </a>
         <a href="https://github.com/Matamir" class="text-white" target="_blank">
-          <i class="fab fa-github"></i>
+          <i class="fab fa-github"> github</i>
         </a>
       </div>
 
