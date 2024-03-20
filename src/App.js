bc1ed6b470d8db6ce6d4f90524d3b6ade446e8d6
Matamir <mhirsch217@gmail.com>
2024-03-20 15:36:52 -0400

    404 page for Netlify

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import Navbar from './components/navbar';
 import About from './components/about';
 import ProjectPage from './components/projectpage';
+import PageNotFound from './components/404';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route exact path="/" element={<About/>} />
         <Route path="/projectsPage" element={<ProjectPage/>}/>
         <Route path="/demos"/>
+        <Route path="/404" element={<PageNotFound/>}/>
       </Routes>
     </Router>
   );
---
+++ src/components/404.js
@@ -0,0 +1,11 @@
+const PageNotFound = () => {
+
+  return(
+    <div>
+        <div className="container" style={{paddingTop:"150px"}}>
+            Page Not Found
+        </div>
+    </div>
+  )
+}
+export default PageNotFound;
